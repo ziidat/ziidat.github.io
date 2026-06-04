9a7947b568ccdc5e279c496b61796afc00726e24
Achmad Fauzi <achmadfauzi987a@gmail.com>
2026-06-04 21:54:11 +0700

    SEO Optimize

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://ziidat.github.io',
